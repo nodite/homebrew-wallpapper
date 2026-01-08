2b332ede773f5caf9ccd5a324bf9ac6a8b1c9866
Oscaner Miao <oscaner1997@gmail.com>
2026-01-08 10:42:23 +0800

    chore: update repository URLs and modernize Homebrew formula
    - Change repository URLs from mczachurski to nodite organization
    - Remove macOS Mojave version requirement dependency
    - Modernize depends_on syntax to use Ruby hash notation (xcode: :build)

--- wallpapper.rb
+++ wallpapper.rb
@@ -1,14 +1,13 @@
 require 'formula'
 
 class Wallpapper < Formula
-  homepage 'https://github.com/mczachurski/wallpapper'
-  url 'https://github.com/mczachurski/wallpapper/archive/1.7.4.tar.gz'
+  homepage 'https://github.com/nodite/wallpapper'
+  url 'https://github.com/nodite/wallpapper/archive/1.7.4.tar.gz'
   sha256 '5d099a7b09948307ccab4aca046e90ef5456c13cb826498a5a871205adb57afd'
 
-  head 'https://github.com/mczachurski/wallpapper.git'
+  head 'https://github.com/nodite/wallpapper.git'
 
-  depends_on :xcode => :build
-  depends_on :macos => :mojave
+  depends_on xcode: :build
 
   def install
     system 'swift', 'build', '--disable-sandbox', '--configuration', 'release'
